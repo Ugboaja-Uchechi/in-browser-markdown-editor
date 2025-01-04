afc7ddc82857c3fa517fe1a63a24eac8e70fb81f
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2025-01-04 14:47:53 +0100

    Remove comments

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-// var showdown = require('showdown')
-// text = '# hello, markdown!',
-// html = converter.makeHtml(text);
-
 
 
 function run() {
 
   const text = document.getElementById('sourceTA').value;
-  // Perform the conversion
   const preview = document.querySelector('#targetDiv');
   const converter = new showdown.Converter();
   let convertedText = converter.makeHtml(text);
   preview.innerHTML = convertedText;
 
-  // var converter = new showdown.Converter(),
-  //   text = document.getElementById('sourceTA').value
-  // html = converter.makeHtml(text);
-
-  
-  // console.log('hello', html)
-  // document.getElementById('targetDiv').innerHTML = html;
 }
